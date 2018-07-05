3678f736e9d3449888916dadfbf8e793a4448b94
Florian <mail@flo-schoen.de>
2018-07-05 10:56:51 +0200

    feat:termine von index.php in eigene Datei ausgelagert

---
+++ termine.php
@@ -0,0 +1,36 @@
+<!-- <section class="box">
+    <h3>Termine</h3>
+	<ul class="alt">
+		<li>
+			<details>
+			    <summary>
+				    <span class="floating-box">24.03.2018</span> 
+				    <span class="floating-box">Spiel ohne Grenzen</span> 
+				    <a href="#" class="button alt small floating-box-right">Teilnehmen</a>
+			    </summary>
+			    <p style="margin: .5em 0 .5em 1.5em"><b>Treffpunkt: 10 Uhr an der Hütte</b></br>
+			        Wir gestalten die Schönbuchstrolche einen Schnuppertag unter dem 
+                    Motto: "Spiel ohne Grenzen" mit Kräuterwanderung und Dino-Mitmachgeschichte.</p>  	
+            </details>
+        </li>
+		<li>
+            <details>
+				<summary>
+					<span class="floating-box">21.04.2018 </span> 
+					<span class="floating-box">Waldeinsatz</span>
+				</summary>
+				<p>bla bla</p>
+			</details>
+		</li>	
+		<li>
+			<details>
+				<summary>
+					<span class="floating-box">01.05.2018</span> 
+					<span class="floating-box">Dorffest</span> 
+					<a href="#" class="button alt small floating-box-right">Teilnehmen</a>
+				</summary>
+			</details>
+		</li>
+    </ul>
+</section>
+//-->
