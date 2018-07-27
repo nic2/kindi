7e1a69330bfaa52e567a82161e6c92e231e304db
Florian <mail@flo-schoen.de>
2018-07-27 22:05:10 +0200

    imwald.php aus imwld.html erstellt

---
+++ ImWald.php
@@ -0,0 +1,98 @@
+<?php 
+
+# imWald.php
+# Beschreibung eines tpyischen Tages im Wald bei den Schönbuchstolchen
+# Erstellt 2018: Nicole und Florian Schön
+
+?>
+
+<!DOCTYPE HTML>
+<!-- 
+    Schönbuchstrolche Waldkindergarten Hildrizhausen e.V.
+    (C) 2018 
+-->
+<html>
+	<head>
+		<title>Schönbuchstrolche - Ein Tag im Wald</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+		<div id="page-wrapper">
+
+			<!-- Header -->
+            <header id="header" class="alt">
+					<?php
+						include('./header.html')
+					?>
+			</header>
+            <section id="generic_banner">
+					
+			</section>
+			<!-- Main -->
+	        </div>
+				<section id="main" class="container">
+					<header>
+						<h2>Im Wald</h2>
+					</header>
+					<div class="box">
+						<p>
+							Der tägliche Aufenthalt im Wald zu jeder Jahreszeit und bei jedem Wetter (außer bei extremen Wetterlagen) gibt den Kindern Raum mit der sie umgebenden Natur unmittelbat in Kontakt zu kommen. Das Erwerben von Sachwissen, das Beobachten von unterschiedlichsten Naturphänomenen und dadurch eine große Sensibilisierung für die Welt um das Kind herum sind wichtige Aspekte für uns. Die Freude an der Bewegung, die individuelle Entfaltung der Phantasie werden intensiv ermöglicht. 
+						</p>
+						<p>
+							Der Wald bietet viel Platz zum Rennen, Klettern, Toben und Springen. Die Kinder nehmen ihre Umgebung mit allen Sinnen wahr. Sie erkennen unterschiedliche Vögel am Gesang, riechen harzige Nadelbäume, hören den Wind in den Blättern der Bäume rascheln und fühlen den Waldboden unter den Füßen mit weichen Moosflechten oder sandigen Kuhlen. 
+							Je nach Waldplatz und Witterung kann gemalt, geschnitzt, gebastelt, gebaut oder geplanscht werden. 
+						</p>
+
+						<h3>Unsere Ausrüstung im Wald</h3>
+						<div class="clearfix">
+							<img class="image left" src="images/pic02.jpg" alt="" />
+							<p>
+								Dürfen wir vorstellen: Die Karre! Dieses Gefährt ist immer dabei wenn die Gruppe einen der Waldplätze ansteuert. Darin befindet sich alles was die Strolche für einen Vormittag im Wald brauchen, wie z.B. Sitzkissen, der Wasserkanister zum Händewaschen und an Geburtstagen sind ganze Dekorationsausstattungen inklusive Geburtstagsschnecke mit an Bord. 
+								Ansonsten findet auch noch Werkzeug oder weiteres Material für die jeweils geplanten Aktivitäten Platz.
+								Natürlich ist darin auch alles was hoffentlich nicht gebraucht wird, nämlich der Verbandskasten und das Notfallhandy.
+							</p>
+						</div>
+						<div class="clearfix">
+							<img class="image right" src="images/pic02.jpg" alt="" />
+							<p>
+								Außerdem ist das tragbare Klo, 'Heinrich' genannt, immer mit dabei. Im Wald wird es auf eine kleine Grube gestellt und in einer diskreten Ecke platziert. Fast wie ein normales Klo - nur mit interessanterem Rundumblick.
+							</p>
+						</div>
+						<div class="clearfix">
+							<p>
+								Volle Fahrt voraus!
+							</p>
+						</div>
+						<div class="clearfix">
+							<p>
+								< TODO Hier vielleicht noch exemplarisch verschiedene Aktivitäten an den Waldplätzen mit Bildern >
+							</p>
+						</div>
+					</div>
+				</section>
+
+			<!-- Footer -->
+			<!-- Footer -->
+            <footer id="footer">
+                    <?php
+						include('./footer.html')
+					?>
+			</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollgress.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
