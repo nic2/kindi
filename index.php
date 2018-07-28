9ff0d580b6db90062d1703380c51963dea57d0b8
Florian <mail@flo-schoen.de>
2018-07-28 15:18:11 +0200

    feat:bild für unsere Hütte angepasst

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
 
 					<section class="box special">
 						<section class="spotlight left">
-							<a href="unsereHuette.php"><img class="image" src="images/pic01.jpg" alt="" /></a>
+							<a href="unsereHuette.php"><img class="image" src="images/huette.jpg" alt="" /></a>
 							<div class="content">
 								<h3>Unsere Hütte</h3>
 								<p>Zu unserem Waldkindergarten gehört eine Blockhütte mit angrenzendem Spielbereich. Diese dient als Rückzugsort bei sehr nassem, kaltem oder windigem Wetter. Hier gibt es auch einen warmen Holzpelettofen, eine Küche und ein Bad mit Toilette.</p>
