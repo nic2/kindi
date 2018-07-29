99632c317e1e7ca87705e26d2a55869d86dab98a
Florian <mail@flo-schoen.de>
2018-07-29 20:25:20 +0200

    feat: impressum + entsprechende klasse bereitgestellt

---
+++ impressum.php
@@ -0,0 +1,100 @@
+<?php 
+
+# impressum.php
+# Impressum für die Webseite
+# Erstellt 2018: Nicole und Florian Schön
+
+?>
+
+<!DOCTYPE HTML>
+<!-- 
+    Schönbuchstrolche Waldkindergarten Hildrizhausen e.V.
+    (C) 2018 
+-->
+<html>
+	<head>
+		<title>Schönbuchstrolche - Impressum</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+		<div id="page-wrapper">
+
+			<!-- Header -->
+            <header id="header" class="alt">
+					<?php
+						include('./header.html')
+					?>
+			</header>
+            <section id="generic_banner">
+					
+			</section>
+			<!-- Main -->
+	        </div>
+				<section id="main" class="container">
+					<header>
+						<h2>Impressum</h2>
+					</header>
+					<div class="box">
+                        <h3>Angaben gemäß § 5 TMG</h3>
+                        <h4>Name und Anschrift</h4>
+						<div class="box impressum">
+                        Schönbuchstrolche Waldkindergarten Hildrizhausen e. V. </br>
+                        Am Kohltor</br>
+                        71157 Hildrizhausen</br>
+                        &nbsp;</br>
+                        Telefon: +49 (160) 7033123</br>
+                        &nbsp;</br>
+                        E-Mail: <a href="mailto:info@schoenbuchstrolche.de"> info@schoenbuchstrolche.de</a></br>
+                        Internet: <a href="https://www.schoenbuchstrolche.de">www.schoenbuchstrolche.de</a>
+                        </div>
+                        <h4>Vertretungsberechtigter Vorstand</h4>
+                        <div class="box impressum">
+                            Verena Heuberger (1. Vorstand)</br>
+                            Alexander Bichler (Schatzmeister)</br>
+                            &nbsp;</br>
+                            Registergericht: Registergericht Stuttgart  </br>
+                            Registernummer VR 241523 </br>
+                            &nbsp; </br>
+                            Umsatzsteuer-Identifikationsnummer gemäß §27 a Umsatzsteuergesetz: ???
+                        </div>
+                        <h4> Bankverbindung </h4>
+                        <div class="box impressum">  
+                            Kreissparkasse Böblingen </br>
+                            IBAN: DE11 6035 0130 0002 3113 64 </br>
+                            BIC: BBKRDE6BXXX </br>
+                        </div>
+                        <h4> Webseite </h4>
+                        <div class="box impressum">
+                            Design und Gestaltung der Seite: Nicole und Florian Schön, Altdorf </br>
+                            Verantwortlich für den Inhalt: Florian Schön </br>
+                        </div>
+                        <h4>Haftungshinweis</h4>
+                        <div class="box impressum">
+                            Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die Inhalte externer Links. Für den Inhalt der verlinkten Seiten sind ausschließlich deren Betreiber verantwortlich.
+                        </div>
+					</div>
+				</section>
+			<!-- Footer -->
+            <footer id="footer">
+                    <?php
+						include('./footer.html')
+					?>
+			</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollgress.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
