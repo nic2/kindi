cca968aac48e729fa847f9388a706168c9dd6da3
Florian <mail@flo-schoen.de>
2018-07-31 22:44:26 +0200

    feat: text update bilder update

--- TagImWald.php
+++ TagImWald.php
@@ -42,21 +42,19 @@
 							Kindergartenkinder brauchen Rituale: im Naturraum genauso wie in Regeleinrichtungen. Rituale geben Orientierung, Sicherheit, Geborgenheit und stärken das Gemeinschaftsgefühl. Deshalb ist unser Tagesablauf in seiner wesentlichen Struktur immer gleich.
 						</p>	
 						<div class="clearfix">
-							<img class="image right" src="images/pic02.jpg" alt="" />
+							<img class="image right" src="images/ankommen.jpg" alt="Kinder kommen an" />
 							<h4>7.30 Uhr bis 8.30 Uhr Bringzeit</h4>
 							<p>
 								Eltern bringen ihre Kinder an die Hütte, dort werden sie von den Erzieherinnen empfangen und es ist Raum für einen kurzen Austausch. Die Kinder treffen ihre Freunde und es werden erste Spielideen ausgetauscht.
 							</p>
 						</div>
 						<div class="clearfix">
-							<img class="image left" src="images/pic02.jpg" alt="Kinder sitzen im Morgenkreis" />
+							<img class="image left" src="images/morgenkreis.jpg" alt="Kinder sitzen im Morgenkreis" />
 							<h4>8.30 Uhr Morgenkreis</h4>
 							<p>	
-								Zu Beginn des Morgenkreises singen wir zunächst ein Begrüßungslied. Dann besprechen wir wer hier ist, wer fehlt und wer weiß warum. Anschließend zählen wir gemeinsam die Kinder. Hierbei lernen sie ganz spielerisch den Umgang mit Zahlen.
-							</p>
-							<p>	
+								Zu Beginn des Morgenkreises singen wir zunächst ein Begrüßungslied. Dann besprechen wir wer hier ist, wer fehlt und wer weiß warum. Anschließend zählen wir gemeinsam die Kinder. Hierbei lernen sie ganz spielerisch den Umgang mit Zahlen.</br>&nbsp;</br>
 								Im Morgenkreis
-								<ul>
+								<ul class="inbox">
 									<li>lernen wir Lieder, Fingerspiele, hören eine Geschichte passend zum aktuellen Thema</li>
 									<li>entscheiden wir gemeinsam was wir an diesem Tag tun wollen</li>
 									<li>erzählen die Kinder etwas Persönliches</li>
@@ -65,14 +63,14 @@
 							</p>
 						</div>
 						<div class="clearfix">
-							<img class="image left" src="images/pic02.jpg" alt="" />
+							<img class="image right" src="images/weginwald.jpg" alt="Weg in den Wald" />
 							<h4>9.00 Uhr Weg in den Wald</h4>
 							<p>
 								Als Ziel haben wir verschiedene Plätze zur Auswahl, welche wir abhängig von der Witterung, der Jahreszeit bzw. der geplanten pädagogischen Aktivität besuchen. Im Sommer ist es z.B. der Kohlweiher und im Winter ein Hang zum Herunterrutschen.Unterwegs ist Zeit zum Erkunden, zum Beobachten, zum Fragen: Eine Blume hier, ein Vogel dort. Federn, Fichtennadeln, ein angenagter Zapfen, ein Frosch, … 
 							</p>
 						</div>
 						<div class="clearfix">
-							<img class="image right" src="images/pic02.jpg" alt="" />
+							<img class="image left" src="images/freispiel.jpg" alt="Freispiel" />
 							<h4>9.30 Uhr Ankommen am Waldplatz, Beginn der Freispielzeit</h4>
 							<p>Der Wald setzt natürliche Grenzen und bietet viele Freiheiten: die Freiheit mit anderen zu kooperieren, sich zurückzuziehen oder einem anderen auch mal aus dem Weg zu gehen.
 So wirkt der Wald positiv auf das Konfliktverhalten der Kinder und es entstehen weniger Aggressionen.
@@ -87,7 +85,7 @@ Stöcke, Zapfen, Erde, Blätter, Wurzeln, Steine,...Hier sind die Phantasie und 
 							</p>
 						</div>
 						<div class="clearfix">
-							<img class="image left" src="images/pic02.jpg" alt="" />
+							<img class="image right" src="images/vesper.jpg" alt="Vesper am Waldsofa" />
 							<h4>10.15 Uhr Vesperzeit</h4>
 							<p>
 								Nach dem gründlichen Händewaschen, bei dem die Kinder ganz selbstverständlich eine Aufgabe übernehmen (Förderung der Selbständigkeit u.a.), wird gemeinsam gefrühstückt.
@@ -95,10 +93,10 @@ Zum Vesper haben wir selbst etwas mitgebracht. Tee und im Sommer Sprudel sind im
 							</p>
 						</div>
 						<div class="clearfix">
-							<img class="image right" src="images/pic02.jpg" alt="" />
+							<img class="image left" src="images/aktivitaet.jpg" alt="Singkreis als gezielte Aktivität" />
 							<h4>10.45 Uhr Gezielte Aktivität mit der Gesamtgruppe oder in Altersgruppen</h4>
 							<p>Es finden gezielte Aktivitäten und Projekte zu verschiedenen Bildungsthemen statt bei deren Auswahl die Bedürfnisse der Kinder eine Rolle spielen, aber auch die Relevanz für ihre ganzheitliche Entwicklung.
-								<ul>
+								<ul class="inbox">
 									<li>rhythmische Erziehung (Trommeln, tanzen, singen)</li>
 									<li>Sprachförderung (Theater, Bilderbücher)</li>
 									<li>Künstlerische und handwerkliche Förderung</li>
@@ -109,26 +107,23 @@ Zum Vesper haben wir selbst etwas mitgebracht. Tee und im Sommer Sprudel sind im
 							</p>
 						</div>
 						<div class="clearfix">
-								<img class="image left" src="images/pic02.jpg" alt="" />
+								<img class="image right" src="images/abschlusskreis.jpg" alt="Abschlusskreis" />
 								<h4>11.30 Uhr Aufräumen und Abschlusskreis</h4>
 								<p>
 									Nach dem Aufräumen findet ein Abschlusskreis statt, dort haben die Kinder die Möglichkeit von besonderen Ereignissen zuberichten oder Selbstgebasteltes den anderen Kindern zu zeigen. Wir besprechen was für morgen geplant ist und singen ein Abschlusslied.
 								</p>
 						</div>
 						<div class="clearfix">
-								<img class="image right" src="images/pic02.jpg" alt="" />
+								<img class="image left" src="images/rueckweg.jpg" alt="Rueckweg" />
 								<h4>12:00 Uhr Rückweg</h4>
 								<p>
 									Wir gehen mit dem Bollerwagen wieder zurück zur Hütte.
 								</p>
-						</p>
-						</div>
-						<div class="clearfix">
-								<img class="image left" src="images/pic02.jpg" alt="" />
 								<h4>12:30 Uhr bis 13:30 Uhr Abholzeit</h4>
 								<p>
 									Die Kinder werden von ihren Eltern an der Hütte abgeholt. Zeit für einen kurzen Austausch, was die einzelnen Kinder heute im Wald erlebt haben.
 								</p>
+						</p>
 						</div>
 					</div>
 
@@ -138,10 +133,10 @@ Zum Vesper haben wir selbst etwas mitgebracht. Tee und im Sommer Sprudel sind im
 							Zweimal die Woche haben die Eltern die Möglichkeit ihre Kinderdurchgängig bis 17.00Uhr betreuen zu lassen. 
 						</p>
 						<div class="clearfix">
-								<img class="image left" src="images/pic02.jpg" alt="" />
+								<img class="image left" src="images/essen.jpg" alt="" />
 								<h4>13.30 Uhr Mittagessen</h4>
 								<p>
-									Das Mittagessen wird uns frisch von Robin Cook an die Hütte geliefert.
+									Das Mittagessen wird uns frisch von Meyers Menu an die Hütte geliefert.
 								</p>
 						</div>
 						<div class="clearfix">
@@ -152,20 +147,16 @@ Zum Vesper haben wir selbst etwas mitgebracht. Tee und im Sommer Sprudel sind im
 							</p>
 						</div>
 						<div class="clearfix">
-							<img class="image left" src="images/pic02.jpg" alt="" />
+							<img class="image left" src="images/nachmittagsaktivitaet.jpg" alt="NAchmittagsaktivitaet" />
 							<h4>15.00 Uhr bis 17.00 Uhr, Aktivitäten</h4>
 							<p>
-								Evtl. gehen wir noch einmal in den Wald oder bieten den Kindern an derHütte verschiedene Angebote an wie z.B. Tonen, Malen, Werken, …
+								Evtl. gehen wir noch einmal in den Wald oder bieten den Kindern an der Hütte verschiedene Angebote an wie z.B. Tonen, Malen, Werken, …
 							</p>
-						</div>
-						<div class="clearfix">
-							<img class="image right" src="images/pic02.jpg" alt="" />
 							<h4>16.30 Uhr bis 17.00 Uhr Abholzeit</h4>
 							<p>
 								Die Eltern kommen an die Hütte und holen ihre müden aber zufriedenen Kinder ab.
 							</p>
 						</div>
-
 					</div>
 				</section>
 
