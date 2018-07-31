ad4a5424af35b376e5f983ab9915729eee4238b6
Florian <mail@flo-schoen.de>
2018-07-31 22:44:10 +0200

    feat: text update, bilder update

--- ImWald.php
+++ ImWald.php
@@ -40,18 +40,22 @@
 					</header>
 					<div class="box">
 						<p>
-							Der tägliche Aufenthalt im Wald zu jeder Jahreszeit und bei jedem Wetter (außer bei extremen Wetterlagen) gibt den Kindern Raum mit der sie umgebenden Natur unmittelbat in Kontakt zu kommen. Das Erwerben von Sachwissen, das Beobachten von unterschiedlichsten Naturphänomenen und dadurch eine große Sensibilisierung für die Welt um das Kind herum sind wichtige Aspekte für uns. Die Freude an der Bewegung, die individuelle Entfaltung der Phantasie werden intensiv ermöglicht. 
+							Der tägliche Aufenthalt im Wald zu jeder Jahreszeit und bei jedem Wetter (außer bei extremen Wetterlagen) gibt den Kindern Raum mit der sie umgebenden Natur unmittelbar in Kontakt zu kommen. 
+							Das Erwerben von Sachwissen, das Beobachten von unterschiedlichsten Naturphänomenen und dadurch eine große Sensibilisierung für die Welt um das Kind herum sind wichtige Aspekte für uns. 
+							Die Freude an der Bewegung, die individuelle Entfaltung der Phantasie werden intensiv ermöglicht. 
 						</p>
 						<p>
-							Der Wald bietet viel Platz zum Rennen, Klettern, Toben und Springen. Die Kinder nehmen ihre Umgebung mit allen Sinnen wahr. Sie erkennen unterschiedliche Vögel am Gesang, riechen harzige Nadelbäume, hören den Wind in den Blättern der Bäume rascheln und fühlen den Waldboden unter den Füßen mit weichen Moosflechten oder sandigen Kuhlen. 
+							Der Wald bietet viel Platz zum Rennen, Klettern, Toben und Springen. Die Kinder nehmen ihre Umgebung mit allen Sinnen wahr. 
+							Sie erkennen unterschiedliche Vögel am Gesang, riechen harzige Nadelbäume, hören den Wind in den Blättern der Bäume rascheln und fühlen den Waldboden unter den Füßen mit weichen Moosflechten oder sandigen Kuhlen. 
 							Je nach Waldplatz und Witterung kann gemalt, geschnitzt, gebastelt, gebaut oder geplanscht werden. 
 						</p>
 
 						<h3>Unsere Ausrüstung im Wald</h3>
 						<div class="clearfix">
-							<img class="image left" src="images/pic02.jpg" alt="" />
+							<img class="image left" src="images/karre.jpg" alt="Unsere Karre" />
 							<p>
-								Dürfen wir vorstellen: Die Karre! Dieses Gefährt ist immer dabei wenn die Gruppe einen der Waldplätze ansteuert. Darin befindet sich alles was die Strolche für einen Vormittag im Wald brauchen, wie z.B. Sitzkissen, der Wasserkanister zum Händewaschen und an Geburtstagen sind ganze Dekorationsausstattungen inklusive Geburtstagsschnecke mit an Bord. 
+								Dürfen wir vorstellen: Die Karre! Dieses Gefährt ist immer dabei wenn die Gruppe einen der Waldplätze ansteuert. Darin befindet sich alles was die Strolche für einen Vormittag oder auch Nachmittag im Wald brauchen, 
+								wie z.B. Sitzkissen, der Wasserkanister zum Händewaschen und an Geburtstagen sind ganze Dekorationsausstattungen inklusive Geburtstagsschnecke mit an Bord. 
 								Ansonsten findet auch noch Werkzeug oder weiteres Material für die jeweils geplanten Aktivitäten Platz.
 								Natürlich ist darin auch alles was hoffentlich nicht gebraucht wird, nämlich der Verbandskasten und das Notfallhandy.
 							</p>
@@ -59,24 +63,31 @@
 						<div class="clearfix">
 							<img class="image right" src="images/pic02.jpg" alt="" />
 							<p>
-								Außerdem ist das tragbare Klo, 'Heinrich' genannt, immer mit dabei. Im Wald wird es auf eine kleine Grube gestellt und in einer diskreten Ecke platziert. Fast wie ein normales Klo - nur mit interessanterem Rundumblick.
+							In der Karre auch immer mit dabei ist unser tragbares Klo, auch 'Heinrich' genannt. Am Waldplatz angekommen wird es in kleiner diskreten Ecke auf eine frisch ausgehobene kleine Grube gestellt. 
+							Fast wie ein normales Klo - nur mit interessanterem Rundumblick.
 							</p>
-						</div>
-						<div class="clearfix">
 							<p>
 								Volle Fahrt voraus!
 							</p>
 						</div>
 						<div class="clearfix">
+							<img class="image left" src="images/kohlweiher.jpg" alt="" />
+							<h3>Unsere Waldplätze</h3>
 							<p>
-								< TODO Hier vielleicht noch exemplarisch verschiedene Aktivitäten an den Waldplätzen mit Bildern >
+							Im Wald haben wir verschiedene regelmäßige Anlaufstellen, unsere Waldplätze. Je nach Wetter, Jahreszeit und geplanten Aktivitäten wählen die Erzieher einen unsere Waldplätze und 
+							unterschiedliche Rahmenbedingungen. Einige unserer Waldplätze sind:
+							<ul class="inbox">
+									<li>Hexenplatz</li>
+									<li>Zwergenwäldchen</li>
+									<li>Mondrakete</li>
+									<li>Schnitzplatz</li>
+									<li>Sandgrube</li>
+									<li>Kohlweiher</li>
+								</ul>
 							</p>
 						</div>
 					</div>
 				</section>
-
-			<!-- Footer -->
-			<!-- Footer -->
             <footer id="footer">
                     <?php
 						include('./footer.html')
