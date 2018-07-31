41908fa43ebaaa7c33539ef2b47e18ebd9ebae2b
Florian <mail@flo-schoen.de>
2018-07-31 22:44:45 +0200

    feat: seite erstellt und mit leben gefüllt

---
+++ unserWaldkindergarten.php
@@ -0,0 +1,102 @@
+<?php 
+
+# unserWaldkindergarten.php
+# Eine Beschreibung über den Waldkindergarten im Allgemeinen und die SChönbuchstrolche im speziellen
+# Erstellt 2018: Nicole und Florian Schön
+
+?>
+
+<!DOCTYPE HTML>
+<!-- 
+    Schönbuchstrolche Waldkindergarten Hildrizhausen e.V.
+    (C) 2018 
+-->
+<html>
+	<head>
+		<title>Schönbuchstrolche - Ein Tag im Wald</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+		<div id="page-wrapper">
+
+			<!-- Header -->
+            <header id="header" class="alt">
+					<?php
+						include('./header.html')
+					?>
+			</header>
+            <section id="generic_banner">
+					
+			</section>
+			<!-- Main -->
+	        </div>
+				<section id="main" class="container">
+					<header>
+						<h2>Unser Waldkindergarten</h2>
+					</header>
+					<div class="box">
+                        <h3> Waldkindergarten</h3>
+                        <p>
+                           Das Konzept der Waldkindergärten kommt aus Dänemark und hat schon seit Anfang der Siebziger in Deutschland Fuß gefasst. Die Waldkindergärten sind dabei in der ursprünglichen Idee reguläre, staatlich anerkannte 
+                           Kindergärten die nicht an ein Gebäude oder Räumlichkeiten gebunden sind. Meist stehen ein Bauwagen oder eine Schutzmöglichkeit zur Verfügung. Die Schönbuchstrolche ist einer der ersten Waldkindergärten auf der Schönbuchlichtung 
+                           den es schon seit 2002 gibt. Der Kindergarten wird durch den Verein Waldkindergarten Schönbuchstrolche Hildrizhausen e.V. getragen. Maximal 20 Kinder im Alter von 3 bis Schuleintritt bilden dabei eine Kindergartengruppe.
+                        </p>
+                           <h4>Unsere Kindergruppen</h4>
+                        <p>
+                           Eine zusätzliche Förderung findet einmal wöchentlich in den folgenden altersspezifischen kleinen Gruppen statt: 
+                            <ul>
+                                <li>Maulwurf Gruppe (3 Jährigen)</li>
+                                <li>Igel Gruppe (3-4 Jährigen)</li>
+                                <li>Eichhörnchen Gruppe ( 4-5 Jährigen)</li>
+                                <li>Mäuse Gruppe (Vorschüler)</li>
+                           </ul>
+                        </p>
+                        <h3>Unsere Öffnungszeiten</h3>
+						<p>
+							<table>
+                                <tr><td>Mo-Fr</td><td>7:30 - 13:30</td></tr>
+                                <tr><td>Di + Do</td><td>Möglichkeit der Nachmittagsbetreuung bis 17:00 </td>
+                            </table>
+						</p>
+						<h3>Unsere Hütte</h3>
+						<div class="clearfix">
+							<img class="image left" src="images/huette.jpg" alt="Unsere Hütte" />
+							<p>
+                            Der Waldkindergarten verfügt über eine Blockhütte am Rand des Schönbuchs, die sich direkt an das Gelände des Waldhaus anschließt. Dort sind ein Bad mit Toilette und ein modernen Holzpelletofen zum Heizen der Hütte an 
+                            kalten Tagen ebenso eine Selbstverständlichkeit, wie eine kleine Küche. 
+							</p>
+						</div>
+						<div class="clearfix">
+							<img class="image right" src="images/aussenbereich.jpg" alt="Außenbereich um die Hütte" />
+							<p>
+                                An die Hütte schließt sich ein Spielbereich an, der von den Kindern vor allem an kalten Tagen, in den Bring- und Abholzeiten oder auch bei der Nachmittagsbetreuung genutzt werden kann. Ob Buddeln im Sandkasten, 
+                                Klettern auf dem Kletterbaum oder im Baumhaus, Verstecken im Weidentippi, Bücher anschauen auf der Veranda, ... es bieten sich vielfältige Möglichkeiten sich alleine oder mit anderen zu Beschäftigen. Zum Außenbereich zählen zudem noch ein von den 
+                                Kindern mit angelegtes und gespflegtzes Außenbeet und ein Hassenstall mit unseren zwei tiereischen Mitbewohnern. Natürlich haben die Kinder nicht nur an kalten Tagen auch die Möglichkeit im Inneren der Hütte zu 
+                                spielen, sich auszuruhen, ein Bild zu malen oder anderweitig kreativ tätig zu werden.
+                           </p>
+						</div>
+					</div>
+				</section>
+            <footer id="footer">
+                    <?php
+						include('./footer.html')
+					?>
+			</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollgress.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
