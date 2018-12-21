8934a66e5e277f941834c5364e138b2d490001fa
Florian <mail@flo-schoen.de>
2018-12-21 11:59:33 +0100

    feat:seite erstellt

---
+++ konzept.php
@@ -0,0 +1,65 @@
+<?php 
+
+# konzept.php
+# Konzeption der Schönbuchstroche 
+# Erstellt 2018: Nicole und Florian Schön
+
+?>
+
+<!DOCTYPE HTML>
+<!-- 
+    Schönbuchstrolche Waldkindergarten Hildrizhausen e.V.
+    (C) 2018 
+-->
+<html>
+	<head>
+		<title>Schönbuchstrolche - Impressum</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+		<div id="page-wrapper">
+
+			<!-- Header -->
+            <header id="header" class="alt">
+					<?php
+						include('./header.html')
+					?>
+			</header>
+            <section id="generic_banner">
+					
+			</section>
+			<!-- Main -->
+	        </div>
+				<section id="main" class="container">
+					<header>
+						<h2>Konzeption</h2>
+					</header>
+					<div class="box">
+                        <h3>Organisation und pädagogische Ziele</h3>
+                        Die Konzeption des Waldkindergartens befindet sich gerade in der Übarbeitung. Sobald diese abgeschlossen ist wird sie hier in Teilen als Website und vollständig als pdf zur Ansicht bereitgestellt. Wir bitten um etwas Geduld.
+					</div>
+				</section>
+			<!-- Footer -->
+            <footer id="footer">
+                    <?php
+						include('./footer.html')
+					?>
+			</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollgress.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
