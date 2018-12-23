82115cc75c37d5f104977d08b59c3732190effe4
Florian <mail@flo-schoen.de>
2018-12-23 07:18:17 +0100

    feat:update text verein und elternbeirat

---
+++ verein.php
@@ -0,0 +1,121 @@
+<?php 
+
+# imWald.php
+# Beschreibung eines tpyischen Tages im Wald bei den Schönbuchstolchen
+# Erstellt 2018: Nicole und Florian Schön
+
+?>
+
+<!DOCTYPE HTML>
+<!-- 
+    Schönbuchstrolche Waldkindergarten Hildrizhausen e.V.
+    (C) 2018 
+-->
+<html>
+	<head>
+		<title>Schönbuchstrolche - Ein Tag im Wald</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+		<div id="page-wrapper">
+
+			<!-- Header -->
+            <header id="header" class="alt">
+					<?php
+						include('./header.html')
+					?>
+			</header>
+            <section id="generic_banner">
+					
+			</section>
+			<!-- Main -->
+	        </div>
+				<section id="main" class="container">
+					<header>
+						<h2>Unser Verein</h2>
+					</header>
+					<div class="box">
+                        <h3>Irgendwas</h3>
+                        <p>
+						Der Elternverein Schönbuchstrolche Waldkindergarten Hildrizhausen e.V. ist der Träger des Waldkindergartens in Hildrizhausen. 
+						Er wurde vor mehr als 15 Jahren aus einer Elterninitiative heraus, als einer der ersten auf der Schönbuchlichtung gegründet. Der Verein
+                        </p>
+						<h3>Der Vorstand</h3>
+						<div class="clearfix">
+                            <img class="image left small" src="images/karre.jpg" alt="Unsere Karre" />
+                            <h4>1. Vorsitzende</h4>
+							<p>
+                                Verena Heuberger</br>
+                                In den Kreben 18 </br>
+                                71157 Hildrizhausen </br>
+                                <a href="mailto:kassierer@schoenbuchstrolche.de">kassierer@schoenbuchstrolche.de</a> 
+							</p>
+                        </div>
+                        <div class="clearfix">
+                            <img class="image left small" src="images/karre.jpg" alt="Unsere Karre" />
+                            <h4>Kassier</h4>
+							<p>
+							    Alexander Bichler</br>
+                                Falkentorstraße 9</br>
+                                71157 Hildrizhausen </br>
+                                <a href="mailto:kassierer@schoenbuchstrolche.de">kassierer@schoenbuchstrolche.de</a>
+							</p>
+                        </div>
+                        <div class="clearfix">
+                            <img class="image left small" src="images/vorstand_hahn.gif" alt="Unsere Karre" />
+                            <h4>Beisitzer</h4>
+							<p>
+                                Pamela Hahn-Seiberth</br>
+                                und Klaus Seiberth</br>
+                                Schelmenäckerstraße 3</br>
+                                71157 Hildrizhausen </br>
+                                <a href="mailto:kassierer@schoenbuchstrolche.de">kassierer@schoenbuchstrolche.de</a>
+                            </p>
+                        </div>
+                        <div class="clearfix">
+                            <img class="image left small" src="images/karre.jpg" alt="Unsere Karre" />
+                            <h4>Beisitzer</h4>
+							<p>
+                                Florian Schön </br>
+                                Scheunenweg 21</br>
+                                71155 Altdorf </br>
+                                <a href="mailto:kassierer@schoenbuchstrolche.de">kassierer@schoenbuchstrolche.de</a>
+							</p>
+                        </div>
+                        <h3>Elternbeirat</h3>
+						<div class="clearfix">
+							<p>
+							Der Elternbeirat besteht aus zwei Elternteilen, die aktuell ein Kind im Waldkindergarten haben und vertritt die Interessen der Elternschaft gegenüber dem pädagogischen Team und gegenüber dem Vorstand. 
+							Gemeinsam mit dem pädagogischen Team und dem Vorstand wirkt er somit an wichtigen Entscheidungen über den Kindergartenbetrieb mit. Der Elternbeirat tritt zu diesem Zwecke je nach Bedarf, 
+							jedoch mindestens zweimal jährlich zusammen und berichtet der Elternschaft mindestens einmal im Jahr über seine Tätigkeit. Der Elternbeirat ist zudem mitverantwortlich für die Organisation 
+							und Durchführung einiger Veranstaltungen des Kindergartens wie beispielsweise den Nikolaus u. ä. Des Weiteren liegt die Verantwortung für die Pflege der Hütte (vor allem durch den Hüttengroßputzes) beim Elternbeirat.
+							Näheres über die Aufgaben der Elternbeiräte ergibt sich aus den Richtlinien des
+							Sozialministeriums Baden-Württemberg über die Bildung und die Aufgabe der Elternbeiräte.
+							Die aktuellen Elternbeiräte der Schönbuchstrolche sind Melanie Kleine und Simone Fussnegger.
+							</p>	
+						</div>
+					</div>
+				</section>
+            <footer id="footer">
+                    <?php
+						include('./footer.html')
+					?>
+			</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollgress.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
