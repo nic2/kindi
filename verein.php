7ff8ffa2eb2fc34949a82c8b922353185d96ab00
Florian <mail@flo-schoen.de>
2019-01-04 09:54:01 +0100

    feat: vereinsbeschreibung vollständig überarbeitet

--- verein.php
+++ verein.php
@@ -1,7 +1,7 @@
 <?php 
 
-# imWald.php
-# Beschreibung eines tpyischen Tages im Wald bei den Schönbuchstolchen
+# verein.php
+# Beschreiung des Vereins und Vorstellung von Elternbeirat und Vorstand
 # Erstellt 2018: Nicole und Florian Schön
 
 ?>
@@ -13,7 +13,7 @@
 -->
 <html>
 	<head>
-		<title>Schönbuchstrolche - Ein Tag im Wald</title>
+		<title>Schönbuchstrolche - Der Verein</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
@@ -39,24 +39,39 @@
 						<h2>Unser Verein</h2>
 					</header>
 					<div class="box">
-                        <h3>Irgendwas</h3>
+                        <h3>Allgemein</h3>
                         <p>
-						Der Elternverein Schönbuchstrolche Waldkindergarten Hildrizhausen e.V. ist der Träger des Waldkindergartens in Hildrizhausen. 
-						Er wurde vor mehr als 15 Jahren aus einer Elterninitiative heraus, als einer der ersten auf der Schönbuchlichtung gegründet. Der Verein
-                        </p>
+						Unsere Einrichtung ist ein Angebot des Elternvereins <i>Schönbuchstrolche Waldkindergarten Hildrizhausen e.V.</i> als annerkannter freier Träger der Jugendhilfe in Kooperation mi der Gemeinde Hildrizhausen.
+						Der Verein wurde vor mehr als 15 Jahren im März 2002 aus einer Elterninitiative heraus, als einer der ersten auf der Schönbuchlichtung gegründet. Er schafft die organisatorischen und finanziellen Rahmenbedingungnen 
+						für den Betrieb des Kindergartens. Er besteht aus den Familien der Kinder und Fördermitgliedern. 
+						</p>
+						<h3>Mitmachen</h3>
+						<p>
+						Die Kinder verbringen einen großen Teil des Tages in unserem Waldkindergarten. In einer Atmosphäre der Geborgenheit und des Vertrauens sollen den Kindern vielfältige Möglichkeiten zur
+						Auseinandersetzung mit sich selbst und seiner Umwelt geboten werden. Sie lernen Kinder verschiedener gesellschaftlicher Gruppen und Nationalitäten kennen. Zur frühkindlichen Erziehung und Bildung gehören das Hinführen zu Toleranz, Solidarität,
+						Verantwortungsbewusstsein, Selbständigkeit, Gemeinschaftsfähigkeit und Lernfreude (Näheres zur Konzeption finden sie <a href="./konzept.php"> hier</a>). Dabei ist es uns wichtig, dass die Eltern selbst Ihren Teil
+						zur Gestaltung des Rahmens mit beitragen. 
+						
+						Dies erfolgt beispielsweise über die 30 Arbeitsstunden, die jede Familie im Laufe eines Kindergartenjahres leistet, z.B. durch das Pralinenherstellen für den Weihnachtsmarkt, das 
+						Herrichten der Hütte und der Waldplätze am Hüttentag, ... . Auch die Pfelge/ das Putzen der Hütte über das Jahr hinweg und die Versorgung unserer Hasen gehört zu den elementaren Bestandteilen der Partizipation. 
+						Daneben feiern wir als Verein im Laufe des Jahres verschiedene Feste, wie zum Beispiel das Drachenfest, die die Platform bieten mit anderen Eltern in Kontakt zu kommen und Erfahrungen auszutauschen. Oftmals entwickeln sich Verbundenheit und
+						Freundschaften die weit über die reine Kindergartenzeit hinaus gehen.
+						</p>
 						<h3>Der Vorstand</h3>
+						<p>Die Leitungsaufgaben im Verein werden durch den Vorstand ausgeübt. Der erweiterete Vorstand setzt sich zusammen aus den Vorsitzenden, dem Kassier und mehreren Beisitzern, 
+						die für verschiedene Aufgaben verantwortlich zeichnen. Die Wahl des erweiterten Vorstands erfolgt im Rahmen der Mitgliederversammlung jeweils für ein Jahr. Der erweiterte Vorstand besteht im Moment aus:</p>
 						<div class="clearfix">
-                            <img class="image left small" src="images/karre.jpg" alt="Unsere Karre" />
+                            <img class="image left small" src="images/vorstand_verena.jpg" alt="Verena Heuberger" />
                             <h4>1. Vorsitzende</h4>
 							<p>
                                 Verena Heuberger</br>
                                 In den Kreben 18 </br>
                                 71157 Hildrizhausen </br>
-                                <a href="mailto:kassierer@schoenbuchstrolche.de">kassierer@schoenbuchstrolche.de</a> 
+                                <a href="mailto:vorsitz@schoenbuchstrolche.de">vorsitz@schoenbuchstrolche.de</a> 
 							</p>
                         </div>
                         <div class="clearfix">
-                            <img class="image left small" src="images/karre.jpg" alt="Unsere Karre" />
+                            <img class="image left small" src="images/vorstand_alexander.jpg" alt="Alexander Bichler" />
                             <h4>Kassier</h4>
 							<p>
 							    Alexander Bichler</br>
@@ -66,7 +81,7 @@
 							</p>
                         </div>
                         <div class="clearfix">
-                            <img class="image left small" src="images/vorstand_hahn.gif" alt="Unsere Karre" />
+                            <img class="image left small" src="images/vorstand_pamela.jpg" alt="Pamela Hahn-Seiberth" />
                             <h4>Beisitzer</h4>
 							<p>
                                 Pamela Hahn-Seiberth</br>
@@ -77,22 +92,22 @@
                             </p>
                         </div>
                         <div class="clearfix">
-                            <img class="image left small" src="images/karre.jpg" alt="Unsere Karre" />
+                            <img class="image left small" src="images/karre.jpg" alt="Florian Schön" />
                             <h4>Beisitzer</h4>
 							<p>
                                 Florian Schön </br>
                                 Scheunenweg 21</br>
                                 71155 Altdorf </br>
-                                <a href="mailto:kassierer@schoenbuchstrolche.de">kassierer@schoenbuchstrolche.de</a>
+                                <a href="mailto:oeffentlichkeitsarbeit@schoenbuchstrolche.de">oeffentlichkeitsarbeit@schoenbuchstrolche.de</a>
 							</p>
                         </div>
                         <h3>Elternbeirat</h3>
 						<div class="clearfix">
 							<p>
-							Der Elternbeirat besteht aus zwei Elternteilen, die aktuell ein Kind im Waldkindergarten haben und vertritt die Interessen der Elternschaft gegenüber dem pädagogischen Team und gegenüber dem Vorstand. 
+							Neben dem erweiterten Vorstand gibt es ein zweites Gremium innerhalb des Vereins, den Elternbeirat. Er besteht aus zwei Elternteilen, die aktuell ein Kind im Waldkindergarten haben und vertritt die Interessen der Elternschaft gegenüber dem pädagogischen Team und gegenüber dem Vorstand. 
 							Gemeinsam mit dem pädagogischen Team und dem Vorstand wirkt er somit an wichtigen Entscheidungen über den Kindergartenbetrieb mit. Der Elternbeirat tritt zu diesem Zwecke je nach Bedarf, 
 							jedoch mindestens zweimal jährlich zusammen und berichtet der Elternschaft mindestens einmal im Jahr über seine Tätigkeit. Der Elternbeirat ist zudem mitverantwortlich für die Organisation 
-							und Durchführung einiger Veranstaltungen des Kindergartens wie beispielsweise den Nikolaus u. ä. Des Weiteren liegt die Verantwortung für die Pflege der Hütte (vor allem durch den Hüttengroßputzes) beim Elternbeirat.
+							und Durchführung einiger Veranstaltungen des Kindergartens wie beispielsweise den Nikolaus u.ä.. Des Weiteren liegt die Verantwortung für die Pflege der Hütte (vor allem durch den Hüttengroßputzes) beim Elternbeirat.
 							Näheres über die Aufgaben der Elternbeiräte ergibt sich aus den Richtlinien des
 							Sozialministeriums Baden-Württemberg über die Bildung und die Aufgabe der Elternbeiräte.
 							Die aktuellen Elternbeiräte der Schönbuchstrolche sind Melanie Kleine und Simone Fussnegger.
