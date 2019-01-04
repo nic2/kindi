1b62a5aa6b0d8a5421414d11c72c358bc18a23ab
Florian <mail@flo-schoen.de>
2019-01-04 21:08:16 +0100

    feat: job.php angelegt

---
+++ jobs.php
@@ -0,0 +1,65 @@
+<?php 
+
+# jobs.php
+# Informationen über aktuelle Jobs bei den Schönbuchstrolchen
+# Erstellt 2018: Nicole und Florian Schön
+
+?>
+
+<!DOCTYPE HTML>
+<!-- 
+    Schönbuchstrolche Waldkindergarten Hildrizhausen e.V.
+    (C) 2018 
+-->
+<html>
+	<head>
+		<title>Schönbuchstrolche - EJobs</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+		<div id="page-wrapper">
+
+			<!-- Header -->
+            <header id="header" class="alt">
+					<?php
+						include('./header.html')
+					?>
+			</header>
+            <section id="generic_banner">
+					
+			</section>
+			<!-- Main -->
+	        </div>
+				<section id="main" class="container">
+					<header>
+						<h2>Jobs</h2>
+					</header>
+					<div class="box">
+						<p>
+							Im Moment haben wir leider keine offenen Stellen. Vielleicht ein ander mal. Schauen Sie einfach wieder vorbei.
+						</p>
+					</div>
+				</section>
+            <footer id="footer">
+                    <?php
+						include('./footer.html')
+					?>
+			</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollgress.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
